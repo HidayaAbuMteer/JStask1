14ac430eef69d146261e6ab16698b5e237cfc7dd
HidayaAbuMteer <hidayaabumteer@gmail.com>
2025-09-23 12:13:39 +0300

    js task1

---
+++ main.js
@@ -0,0 +1,71 @@
+
+        let cash =1000;
+        let currentLiabilities=500;
+        let casFlowRatio = cash/currentLiabilities;
+        console.log(casFlowRatio);
+        
+        let revenues=1000;
+        let expensive=500;
+        let netIncome=revenues-expensive;
+        console.log(netIncome);
+
+        let liabilities=1000;
+        let equity=500;
+        let totalAssets=liabilities+equity;
+        console.log(totalAssets);
+
+        let profit=1000;
+        let sales=500;
+        let netIncome2=sales*profit;
+        console.log(netIncome2); 
+
+        let number1=7;
+        let number2=9;
+        let number3=2;
+        let average=(number1+number2+number3)/3;
+        console.log(average); 
+        
+        let price=150;
+        let discount=30/100;
+        discount=price-(price * discount);
+        console.log(discount); 
+
+        let age =20;
+        console.log(age >18 && age<30); 
+        
+        let num1=2;
+        let num2= 3;
+        let Exponential=num1**num2;
+        console.log(Exponential); 
+    
+        let numb1=10;
+        let numb2=4;
+        let reminder=numb1%numb2;
+        console.log(reminder); 
+
+        console.log(typeof(100));
+        console.log(typeof(73.9));
+        console.log(typeof(NaN));
+        console.log(typeof("Water"));
+        console.log(typeof(false));
+        console.log(typeof(9 != 11));
+
+        console.log("Orang" +"e");
+        console.log("Orang" -"s");
+        console.log("4" +"8");
+        console.log("4" -"8");
+        console.log("name" +3);
+        console.log("name" -3);
+        console.log(82*"word");
+        console.log(1+"hello");
+        console.log("hello"+1);
+        console.log(1+true);
+        console.log("hello"+true);
+        console.log(typeof(infinity));
+        console.log(1 == '1');
+        console.log(1 === '1');
+
+
+        
+
+
